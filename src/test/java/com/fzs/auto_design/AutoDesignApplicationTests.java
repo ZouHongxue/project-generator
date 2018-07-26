57ba1a4eb696dde16291369f833c66b185a56d07
zouhongxue <zouhongxue@qq.com>
2018-07-27 00:20:43 +0800

    base_package

---
+++ src/test/java/com/fzs/auto_design/AutoDesignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fzs.auto_design;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoDesignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
